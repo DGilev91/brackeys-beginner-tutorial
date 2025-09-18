1cbdb447ca467fc8a7d79455e4938e41582ab67f
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2025-09-18 20:00:39 +0500

    enemy

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
 
-func _process(_delta: float) -> void:
-	pass
+
+const SPEED: int = 60
+var direction: int = 1
+
+func _process(delta: float) -> void:
+	if ray_cast_right.is_colliding():
+		direction = -1
+	elif ray_cast_left.is_colliding():
+		direction = 1
+		
+	position.x += direction * SPEED * delta
