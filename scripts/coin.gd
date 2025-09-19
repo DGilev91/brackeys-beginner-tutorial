1b0dc1ec210c7cbf69cecdd60679c3dea202f667
DESKTOP-Q7U4GGT\DimaG <dimagilev91@gmail.com>
2025-09-19 21:59:23 +0500

    sound

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 @onready var game_manger: Node = %GameManger
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 func _on_body_entered(_body: Node2D) -> void:
 	game_manger.add_point()
-	queue_free()
+	animation_player.play("pickup")
